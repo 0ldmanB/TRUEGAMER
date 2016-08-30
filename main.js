ae986c3cf10801c77e6e64804541ca52236440ee
0ldmanB <davidstephenbarry@gmail.com>
2016-08-30 08:35:30 -0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+	$("#load").click(function(){
+
+			$("#load").text('5000 PTS')
+			$("#navText").text("WARNING: THIS IS TRUE G4MING... UNLEASHED");
+			$("#navBar").css({"animation-name":"flasher", "animation-duration": ".5s", "animation-iteration-count": "infinite"});
+			$("#body").css({"animation-name":"flasher", "animation-duration": ".5s", "animation-iteration-count": "infinite"});
+			$("#trueGamer").attr("src","gamerGamed.png");
+
+		
+		
+	});
+
+});
+
