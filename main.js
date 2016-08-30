db612813f3a8413ff4d8f630922ecc31ad485200
0ldmanB <davidstephenbarry@gmail.com>
2016-08-30 08:52:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
 
 			$("#load").text('5000 PTS')
 			$("#navText").text("WARNING: THIS IS TRUE G4MING... UNLEASHED");
-			$("#navBar").css({"animation-name":"flasher", "animation-duration": ".5s", "animation-iteration-count": "infinite"});
-			$("#body").css({"animation-name":"flasher", "animation-duration": ".5s", "animation-iteration-count": "infinite"});
+			$("#navBar").css({"animation-name":"flasher", "animation-duration": ".3s", "animation-iteration-count": "infinite"});
+			$("#body").css({"animation-name":"flasher", "animation-duration": ".3s", "animation-iteration-count": "infinite"});
 			$("#trueGamer").attr("src","gamerGamed.png");
 
 		
